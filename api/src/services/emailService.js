9c89412255de35c43d93ee2358d3636ef7dfb79a
Lyton <lytonm@lyton.dev>
2024-10-21 17:27:27 -0500

    fix: using template instead of custom html

--- api/src/services/emailService.js
+++ api/src/services/emailService.js
@@ -3,22 +3,32 @@ import dotenv from "dotenv";
 import { emailText, emailHtml } from "../utils/emailConfig.js";
 dotenv.config({ path: "./.env.local" });
 
-const SENDGRID_API_KEY = process.env.SEND_GRID_API_KEY;
+let SENDGRID_API_KEY = "";
 
-if (SENDGRID_API_KEY === undefined) {
+if (process.env.NODE_ENV === "development") {
+  SENDGRID_API_KEY = process.env.SEND_GRID_API_KEY;
+}
+
+if (SENDGRID_API_KEY === "") {
   throw new Error("No SENDGRID_API_KEY found. Add it to your .env.local");
 }
 
 sgMail.setApiKey(SENDGRID_API_KEY);
 
+/**
+ * This is function to send a welcome email to a new user
+ * @param {string} email of the user
+ * @param {string} name of the user
+ * By default, the email is sent from test505@lyton.dev during development
+ */
 const sendWelcomeEmail = (email, name) => {
   sgMail.send({
     to: email,
     from: "test505@lyton.dev",
-    subject:
-      "[Action Required] Account Activation with Pennsylvania Women Work",
-    text: emailText(name),
-    html: emailHtml(name),
+    templateId: "d-7e26b82cf8624bafa4077b6ed73b52bf",
+    dynamicTemplateData: {
+      name: name,
+    },
   });
   console.log("Email sent");
 };
