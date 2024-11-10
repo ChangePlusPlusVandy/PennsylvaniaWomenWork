462d408024deb97f5f53d57b007162d8939abdca
Lyton <lytonm@lyton.dev>
2024-11-09 18:25:09 -0600

    lint: fix prettier trailing comma

--- api/src/server.ts
+++ api/src/server.ts
@@ -20,5 +20,5 @@ app.use("/workshop", routes.workshop);
 connectDB();
 
 app.listen(process.env.PORT || 8000, () =>
-  console.log(`Server running on port ${process.env.PORT || 8000}`)
+  console.log(`Server running on port ${process.env.PORT || 8000}`),
 );
