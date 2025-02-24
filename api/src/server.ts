013de426449027fad4943055ff3d99afb18d3781
Lyton <lytonm@lyton.dev>
2025-02-24 01:00:53 +0000

    fix: add deploy specific cors

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,27 +1,30 @@
 import dotenv from "dotenv";
 import path from "path";
-
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
-
 import express from "express";
 import bodyParser from "body-parser";
 import connectDB from "./config/db";
 import * as routes from "./routes/index";
 import router from "./routes";
 
+dotenv.config({ path: path.resolve(__dirname, "../.env.staging") });
 var cors = require("cors");
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3000" })); // Connect to the frontend PORT 3000
+app.use(cors({
+    origin: process.env.CLIENT_ORIGIN_URL,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 connectDB();
 
-app.use("/user", routes.user);
+app.use("/api/user", routes.user);
 app.use("/api", router);
 
 app.use("/api/workshop", routes.workshop);
 app.use("/api/resource", routes.resource);
 
-app.listen(process.env.PORT || 8000, () => console.log(`Server running on port ${process.env.PORT || 8000}`));
+app.listen(process.env.PORT || 8000, () => console.log(`Server running on port ${process.env.PORT}`));
